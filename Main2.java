ba29d51f85df100b77211128c250531361d642e1
Dan <137257813+ZNackly@users.noreply.github.com>
2024-12-21 11:05:44 +0500

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,40 @@
+// public class Main2 {
+//     public static void main(String[] args) {
+//         System.out.println("Start");
+//         Thread thread = new MyThread();
+//         thread.start();
+//         for (int i = 1; i <= 1000; i++) {
+//             System.out.print("M");
+//         }
+//         System.out.println("\nFinish");
+//     }
+// }
+
+// public class Main2 {
+//     public static void main(String[] args) {
+//         System.out.println("Start");
+//         MyRunnable myRunnable = new MyRunnable();
+//         Thread thread = new Thread(myRunnable);
+//         thread.start();
+//         for (int i = 1; i <= 1000; i++) {
+//             System.out.print("M");
+//         }
+//         System.out.println("\nFinish");
+//     }
+// }
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 1000; i++) {
+                    System.out.print(i + " ");
+                }
+            }
+        });
+        thread.start();
+        System.out.println("\nFinish");
+    }
+}
---
+++ MyRunnable.java
@@ -0,0 +1,16 @@
+// public class MyThread extends Thread {
+//     @Override
+//     public void run() {
+//         for (int i = 1; i <= 1000; i++) {
+//             System.out.print(i + " ");
+//         }
+//     }
+// }
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 1000; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
